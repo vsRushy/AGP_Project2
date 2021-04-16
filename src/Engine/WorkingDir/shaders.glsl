194ff1aac1b81c6aa1613b7318d176bee7f39105
vsRushy <vsRushy@users.noreply.github.com>
2021-04-16 20:12:28 +0200

    New shader.

--- src/Engine/WorkingDir/shaders.glsl
+++ src/Engine/WorkingDir/shaders.glsl
@@ -37,14 +37,22 @@ void main()
 #if defined(VERTEX) ///////////////////////////////////////////////////
 
 layout(location = 0) in vec3 aPosition;
-layout(location = 1) in vec2 aTexCoord;
+// layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoord;
+// layout(location = 3) in vec3 aTangent;
+// layout(location = 4) in vec3 aBitangent;
 
 out vec2 vTexCoord;
 
 void main()
 {
 	vTexCoord = aTexCoord;
-	gl_Position = vec4(aPosition, 1.0);
+
+	float clipping_scale = 5.0;
+
+	gl_Position = vec4(aPosition, clipping_scale);
+
+	gl_Position.z = -gl_Position.z;
 }
 
 #elif defined(FRAGMENT) ///////////////////////////////////////////////
