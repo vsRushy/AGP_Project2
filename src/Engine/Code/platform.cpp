8503bcbb1b8c3277eebe318d2c387d1303012260
vsRushy <vsRushy@users.noreply.github.com>
2021-04-20 20:24:16 +0200

    Mouse zoom.

--- src/Engine/Code/platform.cpp
+++ src/Engine/Code/platform.cpp
@@ -65,7 +65,8 @@ void OnGlfwMouseEvent(GLFWwindow* window, int button, int event, int modifiers)
 
 void OnGlfwScrollEvent(GLFWwindow* window, double xoffset, double yoffset)
 {
-    // Nothing do yet... maybe zoom in/out in the future?
+    App* app = (App*)glfwGetWindowUserPointer(window);
+    app->camera.fov -= (float)yoffset * 2.0f;
 }
 
 void OnGlfwKeyboardEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
