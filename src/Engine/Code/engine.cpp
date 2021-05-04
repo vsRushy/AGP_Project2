d0405b8af5fc5b9ad3e1398b20388d60b0c41501
vsRushy <vsRushy@users.noreply.github.com>
2021-05-04 16:14:55 +0200

    Created dockspace.

--- src/Engine/Code/engine.cpp
+++ src/Engine/Code/engine.cpp
@@ -414,7 +414,125 @@ void Init(App* app)
 
 void Gui(App* app)
 {
-    if (ImGui::BeginMainMenuBar())
+    static bool p_open = true;
+    static bool opt_fullscreen = true;
+    static bool opt_padding = false;
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+    // because it would be confusing to have two docking targets within each others.
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    if (opt_fullscreen)
+    {
+        ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->GetWorkPos());
+        ImGui::SetNextWindowSize(viewport->GetWorkSize());
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    }
+    else
+    {
+        dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+    }
+
+    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+    // and handle the pass-thru hole, so we ask Begin() to not render a background.
+    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+        window_flags |= ImGuiWindowFlags_NoBackground;
+
+    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+    // all active windows docked into it will lose their parent and become undocked.
+    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+    if (!opt_padding)
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("DockSpace Demo", &p_open, window_flags);
+    if (!opt_padding)
+        ImGui::PopStyleVar();
+
+    if (opt_fullscreen)
+        ImGui::PopStyleVar(2);
+
+    // DockSpace
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+    {
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    }
+
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("Options"))
+        {
+            // Disabling fullscreen would allow the window to be moved to the front of other windows,
+            // which we can't undo at the moment without finer window depth/z control.
+            ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+            ImGui::MenuItem("Padding", NULL, &opt_padding);
+            ImGui::Separator();
+
+            if (ImGui::MenuItem("Flag: NoSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
+            if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+            if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
+            if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+            if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+            ImGui::Separator();
+
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMenuBar();
+    }
+
+    ImGui::Begin("Info");
+
+    ImGui::Text("FPS: %f", 1.0f / app->deltaTime);
+
+    ImGui::Separator();
+
+    ImGui::Text("OpenGL version: %s", app->opengl_info.version);
+    ImGui::Text("OpenGL renderer: %s", app->opengl_info.renderer);
+    ImGui::Text("OpenGL vendor: %s", app->opengl_info.vendor);
+    ImGui::Text("OpenGL GLSL version: %s", app->opengl_info.glsl_version);
+
+    ImGui::Separator();
+
+    ImGui::Checkbox("Enable Debug Group Mode", &app->debug_group_mode);
+
+    ImGui::Separator();
+
+    if (ImGui::Button("RenderDoc Capture"))
+    {
+        rdoc_api->TriggerCapture();
+    }
+
+    ImGui::Separator();
+
+    const char* items[] = { "Final Render", "Depth", "Normals" };
+    static const char* current_item = "Final Render";
+
+    if (ImGui::BeginCombo("##combo", current_item))
+    {
+        for (int n = 0; n < IM_ARRAYSIZE(items); n++)
+        {
+            bool is_selected = (current_item == items[n]);
+            if (ImGui::Selectable(items[n], is_selected))
+                current_item = items[n];
+            if (is_selected)
+                ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+    }
+
+    ImGui::End();
+
+    ImGui::End(); // End dockspace
+
+    /*if (ImGui::BeginMainMenuBar())
     {
         if (ImGui::BeginMenu("Menu"))
         {
@@ -441,49 +559,7 @@ void Gui(App* app)
         }
 
         ImGui::EndMainMenuBar();
-    }
-
-    ImGui::Begin("Info");
-
-    ImGui::Text("FPS: %f", 1.0f / app->deltaTime);
-
-    ImGui::Separator();
-
-    ImGui::Text("OpenGL version: %s", app->opengl_info.version);
-    ImGui::Text("OpenGL renderer: %s", app->opengl_info.renderer);
-    ImGui::Text("OpenGL vendor: %s", app->opengl_info.vendor);
-    ImGui::Text("OpenGL GLSL version: %s", app->opengl_info.glsl_version);
-
-    ImGui::Separator();
-
-    ImGui::Checkbox("Enable Debug Group Mode", &app->debug_group_mode);
-
-    ImGui::Separator();
-
-    if(ImGui::Button("RenderDoc Capture")) 
-    {
-        rdoc_api->TriggerCapture();
-    }
-
-    ImGui::Separator();
-
-    const char* items[] = { "Final Render", "Depth", "Normals"};
-    static const char* current_item = "Final Render";
-
-    if (ImGui::BeginCombo("##combo", current_item)) 
-    {
-        for (int n = 0; n < IM_ARRAYSIZE(items); n++)
-        {
-            bool is_selected = (current_item == items[n]);
-            if (ImGui::Selectable(items[n], is_selected))
-                current_item = items[n];
-                if (is_selected)
-                    ImGui::SetItemDefaultFocus();   
-        }
-        ImGui::EndCombo();
-    }
-
-    ImGui::End();
+    }*/
 
     /*ImGui::Begin("Scene");
 
