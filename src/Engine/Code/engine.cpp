c0bf87465f6a4d273c586f4e6e90c63061e43a4b
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2021-05-16 13:21:50 +0200

    all lights good

--- src/Engine/Code/engine.cpp
+++ src/Engine/Code/engine.cpp
@@ -333,8 +333,8 @@ void Init(App* app)
     app->entities.push_back({ TransformPositionRotationScale(vec3(5.0f, 0.0f, -20.0f), 60.0f, vec3(0.0f, 1.0f, 0.0f), vec3(2.0f)),
                               app->patrick_index });
 
-    //app->lights.push_back({ LightType_Point, vec3(0.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 0.0f), vec3(15.0f, 3.0f, -10.0f), 20.0, 1.0, true });
-   // app->lights.push_back({ LightType_Point, vec3(1.0f, 0.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, 10.0f), 10.0, 1.0, true });
+    app->lights.push_back({ LightType_Point, vec3(0.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 0.0f), vec3(15.0f, 3.0f, -10.0f), 20.0, 1.0, true });
+    app->lights.push_back({ LightType_Point, vec3(1.0f, 0.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, 10.0f), 10.0, 1.0, true });
     app->lights.push_back({ LightType_Directional, vec3(1.0f, 1.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f), vec3(0.0f, 10.0f, -3.0f), 0.0f, 0.7f, true });
 
     /* FORWARD RENDERING SHADER */
